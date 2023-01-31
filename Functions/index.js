6cbe6a70678fd4cabe82110327183bb02305720b
Michael <michaeldrpabaya@gmail.com>
2023-01-31 12:22:25 +0800

    functions

---
+++ Functions/index.js
@@ -0,0 +1,26 @@
+// increment decrement
+let number = document.getElementById('number');
+let num = 0
+
+number.innerText = num
+
+function increment() {
+    num++
+    number.innerText = num
+}
+
+function decrement() {
+    num--
+    number.innerText = num
+}
+
+
+// total race
+const lap1 = 21
+const lap2 = 12
+const lap3 = 42
+
+function getTotalRace() {
+    let total = lap1 + lap2 + lap3
+    document.getElementById("race").innerText = total    
+}
